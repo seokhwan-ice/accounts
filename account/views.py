cb96a7b13fd7732ec6e17e322fa814dd8bc3d10b
SH_kim <dal2018@naver.com>
2025-02-16 16:54:44 +0900

    깜빡했던 환경변수설정

--- account/views.py
+++ account/views.py
@@ -13,7 +13,6 @@ from .serializers import (
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-
 class UserCreateView(APIView):
     
     @swagger_auto_schema(
--- myhome/settings.py
+++ myhome/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mri#9+ip97$v5q!nan#-tx)e-6zy%l_sx@_ak&62x*gv+85hol"
-
+load_dotenv()
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
